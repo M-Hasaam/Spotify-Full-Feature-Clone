1716ec0d3df770654f80fd0c5f9ba2869adaef20
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 00:58:04 +0500

    Add files via upload

---
+++ songs/Diljit/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Diljit Dosanjh",
+  description: "Diljit Dosanjh hits",
+  cover: "cover.jpg",
+  songs: [
+  "Afusic - Pal Pal (Official Music Video) Prod. @AliSoomroMusic.mp3",
+  "Anuv Jain - AFSOS ft. AP Dhillon (Official Visualizer).mp3",
+  "Finding Her (Official Video) _ Kushagra  Vanshika Kashyap  Bharat  UR Debut.mp3"
+]
+};
