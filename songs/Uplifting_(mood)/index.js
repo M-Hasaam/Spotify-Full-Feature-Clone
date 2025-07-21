6f4078e2ead685d7370faefec479d57174c6c216
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 02:24:44 +0500

    Add files via upload

---
+++ songs/Uplifting_(mood)/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Get up",
+  description: "You can do it!",
+  cover: "cover.jpg",
+  songs: [
+  "Aditya Rikhari - SAMJHO NA ( NASAMAJH ).mp3",
+  "Jhol  Coke Studio Pakistan  Season 15  Maanu x Annural Khalid.mp3",
+  "Sahiba (Official Music Video) _ Aditya Rikhari, Ankita Chhetri  T-Series.mp3"
+]
+};
