8c183ca2e96b1588d17b2889b9b2138792b63ab1
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 00:46:17 +0500

    Add files via upload

---
+++ songs/cs/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Copyright Songs",
+  description: "Cover Songs for you",
+  cover: "cover.jpg",
+  songs: [
+  "Aditya Rikhari - Paaro (Official Video).mp3",
+  "Afusic - Pal Pal (Official Music Video) Prod. @AliSoomroMusic.mp3",
+  "Sahiba (Official Music Video) _ Aditya Rikhari, Ankita Chhetri  T-Series.mp3"
+]
+};
