d48e77aa8336121f27b788e9cefb9f79fbd07e82
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 00:47:39 +0500

    Add files via upload

---
+++ songs/Dark_(mood)/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Dark Horse",
+  description: "Dark Songs for you",
+  cover: "cover.jpg",
+  songs: [
+  "Aditya Rikhari - Paaro (Official Video).mp3",
+  "Aditya Rikhari - SAMJHO NA ( NASAMAJH ).mp3",
+  "Samjho Na X Wishes - Mashup  Aditya Rikhari  Hasan Raheem  DJ Sumit Rajwanshi.mp3"
+]
+};
