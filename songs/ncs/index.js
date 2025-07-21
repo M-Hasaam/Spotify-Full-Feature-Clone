972437c25c6edc851d40e51b7341af4d7712f89e
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 02:23:50 +0500

    Add files via upload

---
+++ songs/ncs/index.js
@@ -0,0 +1,9 @@
+export const album = {
+  title: "Sleep Songs",
+  description: "Songs for you",
+  cover: "cover.jpg",
+  songs: [
+  "Afusic - Pal Pal (Official Music Video) Prod. @AliSoomroMusic.mp3",
+  "HASEEN - TALWIINDER, NDS, RIPPY (Official Visualizer).mp3"
+]
+};
