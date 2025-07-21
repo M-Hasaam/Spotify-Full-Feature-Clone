d8191a4700e288f1740ad5bcdb55519215d347a5
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 00:59:07 +0500

    Add files via upload

---
+++ songs/Funky_(mood)/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Go Funky",
+  description: "Lets go Funky",
+  cover: "cover.jpg",
+  songs: [
+  "HASEEN - TALWIINDER, NDS, RIPPY (Official Visualizer).mp3",
+  "Jhol  Coke Studio Pakistan  Season 15  Maanu x Annural Khalid.mp3",
+  "Sahiba (Official Music Video) _ Aditya Rikhari, Ankita Chhetri  T-Series.mp3"
+]
+};
