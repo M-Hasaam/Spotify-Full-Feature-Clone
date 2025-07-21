457fa8715b7354e41fe65a53282cc11b502799b8
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-21 18:23:39 +0500

    Add files via upload

---
+++ songs/Chill_(mood)/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Just Chill",
+  description: "Yes, Just Chill",
+  cover: "cover.jpg",
+  songs: [
+  "Finding Her (Official Video) _ Kushagra  Vanshika Kashyap  Bharat  UR Debut.mp3",
+  "HASEEN - TALWIINDER, NDS, RIPPY (Official Visualizer).mp3",
+  "Samjho Na X Wishes - Mashup  Aditya Rikhari  Hasan Raheem  DJ Sumit Rajwanshi.mp3"
+]
+};
