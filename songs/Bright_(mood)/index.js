1ee14fff3a98dbd05291ae8add59440a82a553c0
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 02:26:16 +0500

    Add files via upload

---
+++ songs/Bright_(mood)/index.js
@@ -0,0 +1,17 @@
+export const album = {
+  title: "Bright Songs",
+  description: "Bright Songs for you",
+  cover: "cover.jpg",
+  songs: [
+  "Aditya Rikhari - Paaro (Official Video).mp3",
+  "Aditya Rikhari - SAMJHO NA ( NASAMAJH ).mp3",
+  "Afusic - Pal Pal (Official Music Video) Prod. @AliSoomroMusic.mp3",
+  "Anuv Jain - AFSOS ft. AP Dhillon (Official Visualizer).mp3",
+  "Finding Her (Official Video) _ Kushagra  Vanshika Kashyap  Bharat  UR Debut.mp3",
+  "HASEEN - TALWIINDER, NDS, RIPPY (Official Visualizer).mp3",
+  "Jhol  Coke Studio Pakistan  Season 15  Maanu x Annural Khalid.mp3",
+  "Kaifi Khalil - Kahani Suno 2.0 [Official Music Video].mp3",
+  "Sahiba (Official Music Video) _ Aditya Rikhari, Ankita Chhetri  T-Series.mp3",
+  "Samjho Na X Wishes - Mashup  Aditya Rikhari  Hasan Raheem  DJ Sumit Rajwanshi.mp3"
+]
+};
