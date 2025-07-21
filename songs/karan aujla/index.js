e6fe137d32cfdb5fc39561a3481ef3c635f0c431
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 01:06:31 +0500

    Add files via upload

---
+++ songs/karan aujla/index.js
@@ -0,0 +1,10 @@
+export const album = {
+  title: "Karan Aujla",
+  description: "Karan Aujla for you",
+  cover: "cover.jpg",
+  songs: [
+  "Finding Her (Official Video) _ Kushagra  Vanshika Kashyap  Bharat  UR Debut.mp3",
+  "Kaifi Khalil - Kahani Suno 2.0 [Official Music Video].mp3",
+  "Sahiba (Official Music Video) _ Aditya Rikhari, Ankita Chhetri  T-Series.mp3"
+]
+};
