b2e17c1dc7ce28fa8a12cdef504d089c324db168
Muhammad Hasaam <m.hasaam.official@gmail.com>
2025-07-22 02:22:15 +0500

    Add files via upload

---
+++ songs/Love_(mood)/index.js
@@ -0,0 +1,9 @@
+export const album = {
+  title: "I Love You",
+  description: "Love is in the air",
+  cover: "cover.jpg",
+  songs: [
+  "Anuv Jain - AFSOS ft. AP Dhillon (Official Visualizer).mp3",
+  "Samjho Na X Wishes - Mashup  Aditya Rikhari  Hasan Raheem  DJ Sumit Rajwanshi.mp3"
+]
+};
